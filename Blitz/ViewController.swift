387d76df59e9ffe0ee509481875717e94e5834eb
chowtheprogrammer <zhou267@purdue.edu>
2015-10-01 16:08:26 -0400

    Initial Commit

---
+++ Blitz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Blitz
+//
+//  Created by Mr.Chow on 10/1/15.
+//  Copyright © 2015 cs490. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
