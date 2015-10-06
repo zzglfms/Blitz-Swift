011f7e3a5bdea1c6d75b9ac59520dcdff01151d7
chowtheprogrammer <zhou267@purdue.edu>
2015-10-06 16:39:57 -0400

    Modified entry page depending on login status

--- Blitz/HomeViewController.swift
+++ Blitz/HomeViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     @IBOutlet weak var usernameLabel: UILabel!
     override func viewDidLoad() {
